e93c959a2a9486019afe0dd98be53685841af076
SamuelPerez27 <samuelpperez07@gmail.com>
2021-05-18 20:57:45 -0300

    Creacion de los componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CuerpoComponent } from './component/cuerpo/cuerpo.component';
+import { Error404Component } from './component/error404/error404.component';
+
+
+const routes: Routes = [
+   {path: '', component: CuerpoComponent},
+   {path: '**',component:Error404Component }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeadComponent } from './component/head/head.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { Error404Component } from './component/error404/error404.component';
+import { CuerpoComponent } from './component/cuerpo/cuerpo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadComponent,
+    FooterComponent,
+    Error404Component,
+    CuerpoComponent
   ],
   imports: [
     BrowserModule,
